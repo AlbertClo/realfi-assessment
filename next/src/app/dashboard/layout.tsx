38954198ab29962e2e0ed2a4eff4b37b4411a45e
Albert Cloete <cloete.albert@gmail.com>
2024-06-01 23:00:42 +0200

    Remove unused UI elements.

--- next/src/app/dashboard/layout.tsx
+++ next/src/app/dashboard/layout.tsx
@@ -1,169 +1,102 @@
-'use client'
+"use client";
 
+import createHasuraClient from "@/dataConnections/hasura/client";
+import { ApolloProvider } from "@apollo/client";
 import {
   Disclosure,
   DisclosureButton,
   DisclosurePanel,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
-  Transition
 } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { usePathname } from "next/navigation";
 
 function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
-const user = {
-  name: 'User Name',
-  email: 'name@example.com',
-}
+const hasuraClient = createHasuraClient();
+
 const navigation = [
-  { name: 'Users', href: '/dashboard/users'},
-  { name: 'Overview', href: '/dashboard/overview'},
-]
-const userNavigation = [
-  { name: 'Sign out', href: '#' },
-]
+  {name: "Users", href: "/dashboard/users"},
+  {name: "Overview", href: "/dashboard/overview"},
+];
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathname = usePathname()
+  const pathname = usePathname();
 
   return (
     <div className="min-h-full bg-slate-900">
-      <Disclosure as="nav">
-        {({ open }) => (
-          <>
-            <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-              <div className="border-b border-gray-700">
-                <div className="flex h-16 items-center justify-between px-4 sm:px-0">
-                  <div className="flex items-center">
-                    <div className="hidden md:block">
-                      <div className="flex items-baseline space-x-4">
-                        {navigation.map((item) => (
-                          <a
-                            key={item.name}
-                            href={item.href}
-                            className={classNames(
-                              item.href === pathname
-                                ? 'bg-slate-100 text-slate-900'
-                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                              'rounded px-3 py-2 text-sm font-medium'
-                            )}
-                            aria-current={item.href === pathname ? 'page' : undefined}
-                          >
-                            {item.name}
-                          </a>
-                        ))}
+      <ApolloProvider client={hasuraClient}>
+        <Disclosure as="nav">
+          {({open}) => (
+            <>
+              <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div className="border-b border-gray-700">
+                  <div className="flex h-16 items-center justify-between px-4 sm:px-0">
+                    <div className="flex items-center">
+                      <div className="hidden md:block">
+                        <div className="flex items-baseline space-x-4">
+                          {navigation.map((item) => (
+                            <a
+                              key={item.name}
+                              href={item.href}
+                              className={classNames(
+                                item.href === pathname
+                                  ? "bg-slate-100 text-slate-900"
+                                  : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                                "rounded px-3 py-2 text-sm font-medium"
+                              )}
+                              aria-current={item.href === pathname ? "page" : undefined}
+                            >
+                              {item.name}
+                            </a>
+                          ))}
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="hidden md:block">
-                    <div className="ml-4 flex items-center md:ml-6">
-                      <Menu as="div" className="relative ml-3">
-                        <div>
-                          <MenuButton
-                            className="relative flex max-w-xs items-center rounded bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                            <span className="absolute -inset-1.5"/>
-                            <span className="sr-only">Open user menu</span>
-                            <div className="h-8 w-8 content-center rounded bg-white text-slate-500 font-semibold">AC</div>
-                          </MenuButton>
-                        </div>
-                        <Transition
-                          enter="transition ease-out duration-100"
-                          enterFrom="transform opacity-0 scale-95"
-                          enterTo="transform opacity-100 scale-100"
-                          leave="transition ease-in duration-75"
-                          leaveFrom="transform opacity-100 scale-100"
-                          leaveTo="transform opacity-0 scale-95"
-                        >
-                          <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {userNavigation.map((item) => (
-                              <MenuItem key={item.name}>
-                                {({ focus }) => (
-                                  <a
-                                    href={item.href}
-                                    className={classNames(
-                                      focus ? 'bg-gray-100' : '',
-                                      'block px-4 py-2 text-sm text-gray-700'
-                                    )}
-                                  >
-                                    {item.name}
-                                  </a>
-                                )}
-                              </MenuItem>
-                            ))}
-                          </MenuItems>
-                        </Transition>
-                      </Menu>
+                    <div className="-mr-2 flex md:hidden">
+                      {/* Mobile menu button */}
+                      <DisclosureButton
+                        className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <span className="absolute -inset-0.5"/>
+                        <span className="sr-only">Open main menu</span>
+                        {open ? (
+                          <XMarkIcon className="block h-6 w-6" aria-hidden="true"/>
+                        ) : (
+                          <Bars3Icon className="block h-6 w-6" aria-hidden="true"/>
+                        )}
+                      </DisclosureButton>
                     </div>
                   </div>
-                  <div className="-mr-2 flex md:hidden">
-                    {/* Mobile menu button */}
-                    <DisclosureButton className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="absolute -inset-0.5" />
-                      <span className="sr-only">Open main menu</span>
-                      {open ? (
-                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                      ) : (
-                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                      )}
-                    </DisclosureButton>
-                  </div>
                 </div>
               </div>
-            </div>
-
-            <DisclosurePanel className="border-b border-gray-700 md:hidden">
-              <div className="space-y-1 px-2 py-3 sm:px-3">
-                {navigation.map((item) => (
-                  <DisclosureButton
-                    key={item.name}
-                    as="a"
-                    href={item.href}
-                    className={classNames(
-                      item.href === pathname ? 'bg-slate-100 text-slate-900' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'block rounded-md px-3 py-2 text-base font-medium'
-                    )}
-                    aria-current={item.href === pathname ? 'page' : undefined}
-                  >
-                    {item.name}
-                  </DisclosureButton>
-                ))}
-              </div>
-              <div className="border-t border-gray-700 pb-3 pt-4">
-                <div className="flex items-center px-5">
-                  <div className="h-8 w-8 text-center content-center rounded bg-white text-slate-500 font-semibold">AC</div>
-                  <div className="ml-3">
-                    <div className="text-base font-medium leading-none text-white">{user.name}</div>
-                    <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
-                  </div>
-                </div>
-                <div className="mt-3 space-y-1 px-2">
-                  {userNavigation.map((item) => (
+              <DisclosurePanel className="border-b border-gray-700 md:hidden">
+                <div className="space-y-1 px-2 py-3 sm:px-3">
+                  {navigation.map((item) => (
                     <DisclosureButton
                       key={item.name}
                       as="a"
                       href={item.href}
-                      className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                      className={classNames(
+                        item.href === pathname ? "bg-slate-100 text-slate-900" : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                        "block rounded-md px-3 py-2 text-base font-medium"
+                      )}
+                      aria-current={item.href === pathname ? "page" : undefined}
                     >
                       {item.name}
                     </DisclosureButton>
                   ))}
                 </div>
-              </div>
-            </DisclosurePanel>
-          </>
-        )}
-      </Disclosure>
-      <div>{children}</div>
+              </DisclosurePanel>
+            </>
+          )}
+        </Disclosure>
+        <div>{children}</div>
+      </ApolloProvider>
     </div>
-);
+  );
 }
